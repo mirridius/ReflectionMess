a183905ef656a0c47374d2835fba73174fa6de20
mirridius <mirridius@gmail.com>
2019-03-16 23:52:50 +0300

    basic structure

---
+++ src/main/java/ClientObservable.java
@@ -0,0 +1,8 @@
+import java.util.Observable;
+
+public class ClientObservable extends Observable {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ClientObserver.java
@@ -0,0 +1,8 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class ClientObserver implements Observer {
+    public void update(Observable o, Object arg) {
+
+    }
+}
